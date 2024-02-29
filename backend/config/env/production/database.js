2f5884f1679b3b48414c01e6838a382b8794f751
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-02-29 10:39:45 -0300

    ready?

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,7 +1,4 @@
-const { parse } = require("pg-connection-string");
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => {
-  // const { host, port, database, user, password } = parse(env("DATABASE_URL"));
   
   return {
     connection: {
